07b0d03186a00381a1096bf0c018097f31e4e8ac
fahrstuhl <gabriel.d.zoeller@campus.tu-berlin.de>
2016-02-22 14:35:25 +0100

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.5
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(name='screenAlign',
+        version='1.0',
+        py_modules=['screenAlign'],
+        package_dir = {'': 'lib'},
+        )
