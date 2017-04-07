2230c4c66041bada03a77c3e835341d9e881e7cf
Gabriel Zöller <fahrstuhl@users.noreply.github.com>
2017-04-07 16:24:00 +0200

    increases version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 setup(name='screenAlign',
-        version='1.1',
+        version='1.2',
         py_modules=['screenAlign'],
         author='Fahrstuhl',
         author_email='fahrstuhl@mailbox.tu-berlin.de',
