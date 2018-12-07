23e7b68a8d2abd1bc29d0915e9285da8f876722c
Gabriel Zöller <fahrstuhl@users.noreply.github.com>
2018-12-07 13:33:33 +0100

    bumps version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 setup(name='screenAlign',
-        version='1.3',
+        version='1.4',
         py_modules=['screenAlign'],
         author='Fahrstuhl',
         author_email='fahrstuhl@mailbox.tu-berlin.de',
